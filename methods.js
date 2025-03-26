25610376703a52167701b552c3d00e08f96be8f1
Matthew Mapa <matthewmapa@Matthew-Mapa>
2025-03-26 13:42:53 -0400

    expanded prompt for calendar

--- methods.js
+++ methods.js
@@ -47,6 +47,15 @@ export const methods = [
 
       input['msgs'] = messages;
 
+      const date = new Date();
+
+      const day = date.getDate();
+      const month = date.getMonth() + 1; // The month index starts from 0
+      const year = date.getFullYear();
+      
+      let currentDate = `${day}/${month}/${year}`;
+      console.log(currentDate);
+
       const prompt = ChatPromptTemplate.fromMessages([
               [
                 "system",
@@ -58,8 +67,14 @@ export const methods = [
                Always keep the subject around fitness and subtopics around fitness.
                If subject is not under this scope respond with "Sorry, I can't help with that."
                Offer to create a workout schedule if it makes sense in your answer. If the user requests a schedule, response should include "Here is your proposed schedule:".
-               Make sure any schedule given repeats at a regular interval.
-               If a schedule is created, generate a single google calendar invite. This should always be at the end of the response and formatted as a JSON snippet:
+               Before generating a schedule, ask clarifying questions to determine what equipment the user has available (e.g. full gym, nothing), what their experience is, and
+               when they have time and how much time they have to perform the workouts.
+               Make sure any schedule given repeats at a regular interval. Events in the generated schedule should include the start and end time of the workout.
+               Make sure that the schedule starts after the current date. The current month is ${month}, the current day is ${day}, and the current year is ${year}.
+               After generating a first draft of the schedule, ask the user if there is anything they would like modified. Make sure that the user is ok with the times/days of the week and
+               exercises in the schedule, and if they are not, modify the schedule until the user indicates that they are happy with the proposed schedule.
+               After the user is happy with the schedule, ask the user if they would like you to generate a link that can be added to their Google Calendar. If they say yes,
+               ask the user how many weeks they would like the schedule to go for, then generate a single google calendar invite. This should always be at the end of the response and formatted as a JSON snippet:
                {{
                 "title": "Name of event",
                 "description": "Description",
